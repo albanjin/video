006503d710ceb97410847edb60b8ed052f232b60
albanjin <albanjin@sina.com>
2018-07-06 00:31:03 +0800

    音乐测试案例

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="main-box">
+    <div class="section" v-for='(v,k) in videoArr'>
+      <videoplayer1 :poster="v.poster" :src="v.src"></videoplayer1>
+    </div>
+  </div>
+</template>
+
+<script>
+import videoplayer1 from './videoplayer.vue'
+  export default {
+    components:{
+      videoplayer1
+    },
+    data() {
+      return {
+        videoArr: [
+          {
+            src: "http://video.ivwen.com/users/5291192/1530770605799.mp4",
+            poster: "http://video.ivwen.com/users/5291192/1530770605799.mp4?vframe/jpg/offset/0"
+        },{
+            src: "http://video.ivwen.com/users/13988035/1501987394007.mp4",
+            poster: "http://video.ivwen.com/users/13988035/1501987394007.mp4?vframe/jpg/offset/0"
+        },{
+            src: "http://video.ivwen.com/users/2027075/1505315869187.mp4",
+            poster: "http://video.ivwen.com/users/2027075/1505315869187.mp4?vframe/jpg/offset/0"
+        },{
+            src: "http://video.ivwen.com/users/5291192/1530770605799.mp4",
+            poster: "http://video.ivwen.com/users/5291192/1530770605799.mp4?vframe/jpg/offset/0"
+        },
+        ]
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .main-box {
+    position: relative;
+    width: 100%;
+    .section{
+      position: relative;
+      margin: 40px 0;
+      width:100%;
+    }
+  }
+</style>
